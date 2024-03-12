9dcbda071f752d00becf0d03b0c3e0aa5579b45d
shres <shrestha.sonali@gmail.com>
2024-03-12 20:34:35 +0530

    Validation added

--- src/main/java/com/example/tasktracker/entity/Task.java
+++ src/main/java/com/example/tasktracker/entity/Task.java
@@ -3,6 +3,10 @@ package com.example.tasktracker.entity;
 
 import com.example.tasktracker.enums.TaskState;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,17 +29,23 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     @Column(nullable = false)
+    @NotBlank(message = "Enter the TaskName")
     String taskName;
     @Column(nullable = false)
+    @NotBlank(message = "Enter the Task Description")
     String taskDescription;
     @Column(nullable = true)
+    @NotBlank(message = "Story points can't be zero")
+    @Size(min = 0, max = 100)
     Long storyPoints;
     @Column(nullable = true)
     String assignedUserId;
     @Enumerated(EnumType.STRING)
     @Column(name = "state")
+    @NotNull(message = "Task cannot be null")
     TaskState entityState;
     @Column
+    @FutureOrPresent(message = "")
     private LocalDateTime startDate;
     @Column
     private LocalDateTime targetDate;
--- src/main/java/com/example/tasktracker/entity/User.java
+++ src/main/java/com/example/tasktracker/entity/User.java
@@ -2,6 +2,9 @@ package com.example.tasktracker.entity;
 
 import com.example.tasktracker.enums.UserState;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +12,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(name = "User")
+@Table(name = "t_User")
 @Builder
 @Data
 @AllArgsConstructor
@@ -21,11 +24,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long userId;
     @Column(nullable = false)
+    @NotBlank(message = "Enter the name" )
     String name;
     @Column(nullable = false)
+    @Email(message = "Enter a valid email")
     String email;
     @Column(nullable = false)
+    @NotBlank(message = "Enter a valid phoneNo.")
     String phone;
+    @NotBlank(message = "Mention the status")
     @Column(name = "state")
     UserState state;
 }
